6589c211d6640823abacb6d556b1d2d0d3a02482
Jessie Wong <kimjang.wong1@gmail.com>
2021-04-22 22:34:59 +0000

    updated changes

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-var http = require("http");
-const axios = require("axios").default;
+var http = require("http"),
+axios = require("axios"),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser');
+
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json());
+
+app.get('/hello/:foo/:bar', (req, res) => {
+    res.json({message: 'hi', data: [
+        req.params.foo,
+        req.params.bar
+    ]})
+});
+
+app.post('hello', (req, res) => {
+    res.json({result: 'Post sent'});
+})
 
 //create a server object:
-http.createServer(function(req, res) {
-res.write(users.join("\n")); //display the list of users on the page
-res.end(); //end the response
-})
-.listen(8080); //the server object listens on port 8080
+//http.createServer(function(req, res) {
+//res.write(users.join("\n")); //display the list of users on the page
+//res.end(); //end the response
+//})
+//.listen(8080); //the server object listens on port 8080
 
 let users = [];
 (async function getNames() {
@@ -19,3 +39,7 @@ users = data.map(user => user.name);
 console.log(error);
 }
 })();
+
+app.listen(port, function(err) {
+    console.log('Server listening to port: ' + port);
+});
